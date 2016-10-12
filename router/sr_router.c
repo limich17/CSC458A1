36e9aeab4e7143b37e2632fd628d0266d79015d6
Lovric George <abusale1@cdf.toronto.edu>
2016-10-12 18:59:39 -0400

    1

--- router/sr_router.c
+++ router/sr_router.c
@@ -78,10 +78,12 @@ void sr_handlepacket(struct sr_instance* sr,
 
   printf("*** -> Received packet of length %d \n",len);
   printf("Interface is: %d \n", *interface);
-  printf("Packet: %d \n", *packet);
-  printf("Packet 1: %d \n", *(packet + 1));
-  printf("Packet 2: %d \n", *(packet + 2));
-  printf("Packet 3: %d \n", *(packet + 3));
+  for (int i = 0; i < len; i = i + 1 ){
+	  
+	  printf("Packet 1: %d \n", *(packet + i));
+  }
+  
+	
 
   /* fill in code here */
   
